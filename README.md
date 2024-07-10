66652c4a44fb0730ed676598b0b330de61c8b226
jonmon6691 <jonmon6691@gmail.com>
2024-07-09 20:16:18 -0700

    Initial content

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # arduino_rustyscalling
 Replace the guts of a vintage telephone for fun and... more fun!
+
+## Features
+* Remote ringing!
+* Plays random sound clips when you pick up!
+* Rechargeable!
+* 100% Recycled Jokes!
+
+## Parts
+* **Telephone**: [Connecticut Telephone Inc 1000-JD](https://www.ebay.com/sch/i.html?_nkw=Connecticut%20Telephone&siteid=0&customid=&toolid=10001)
+* **Sound Player**: [Adafruit Audio FX Sound Board + 2x2W Amp - WAV/OGG Trigger - 16MB](https://www.adafruit.com/product/2217)
+* **Arduino**: [Arduino Pro Mini](https://www.sparkfun.com/products/11113)
+* **Boost charger**: [PowerBoost 500 Charger - Rechargeable 5V Lipo USB Boost @ 500mA+](https://www.adafruit.com/product/1944)
+* **Garage Door Opener Module**: [GTO RB709 UNB Universal Receiver - 318Hmz](https://www.gateopenersunlimited.com/gto_receiver_gto_rb709_unb_universal_receiver_gto_garage_door_opener.html)
+* **Remote**: [GTO RB741 Remote Control Two Button Transmitter (Older Model)](https://www.gateopenersunlimited.com/GTO-RB741-Remote-Control-Two-Button-Transmitter--2-Pack_p_254.html)
+* **Ringer Generator**: [Ringing Generator Module and Ringer (Ringing Generator Only)](https://www.modelrailroadcontrolsystems.com/ringing-generator-module-and-ringer/)
+
+# Wiring Schematic
+<img src="docs/wiring.png">
---
+++ arduino_rustyscalling.ino
@@ -0,0 +1,91 @@
+#include <SoftwareSerial.h>
+#include <Adafruit_Soundboard.h>
+
+// SoftwareSerial RX & TX
+#define SFX_TX 8
+#define SFX_RX 9
+
+// RST pin on the Sound Board
+#define SFX_RST 7
+
+// ACT pin on the Sound Board
+// If you call stop while nothing's playing, it will break the board
+#define SFX_ACT 4
+#define ACT_PLAYING 0
+#define ACT_NOT_PLAYING 1
+
+// Hook Switch, phone is off hook when this is 0
+#define HOOK_SW 5
+#define OFF_HOOK 0
+#define ON_HOOK 1
+
+SoftwareSerial ss = SoftwareSerial(SFX_TX, SFX_RX);
+Adafruit_Soundboard sfx = Adafruit_Soundboard(&ss, NULL, SFX_RST);
+
+// Stores the number of files, and incidentally, the length of *playlist
+uint8_t num_files;
+
+// Stores the order to play the song's in, by index
+uint8_t *playlist;
+
+// Stores the track to be played
+uint8_t track_i;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Rustys Calling!");
+  
+  // softwareserial at 9600 baud
+  ss.begin(9600);
+
+  if (!sfx.reset()) {
+    Serial.println("ERROR: SFX Board not found");
+    while (1);
+  }
+
+  num_files = sfx.listFiles();
+  playlist = (uint8_t *) malloc(sizeof(uint8_t) * num_files);
+  // Start with the playlist in order
+  for (int i = 0; i < num_files; i++) playlist[i] = i;
+  init_playlist();
+  track_i = 0;
+
+  pinMode(HOOK_SW, INPUT_PULLUP);
+  pinMode(SFX_ACT, INPUT_PULLUP);
+}
+
+void init_playlist() {
+  // Fisher-yates and Durstenfeld with the good shuffle
+  for (int i = num_files - 1; i > 0; i--) {
+    int j = random(i+1); // int 0 <= j <= i
+    uint8_t tmp = playlist[i];
+    playlist[i] = playlist[j];
+    playlist[j] = tmp;
+  }
+}
+
+void loop() {
+  // Don't need a fast loop
+  delay(100);
+
+  // Always be churning the RNG
+  random(num_files);
+
+  switch (digitalRead(HOOK_SW)) {
+    default:
+    case ON_HOOK:
+      if (digitalRead(SFX_ACT) == ACT_PLAYING) sfx.stop();
+      break;
+
+    case OFF_HOOK:
+      if (digitalRead(SFX_ACT) == ACT_NOT_PLAYING) sfx.playTrack(track_i++);
+      break;
+  }
+
+  // Reshuffle the playlist once we get to the end
+  if (track_i >= num_files) {
+    track_i = 0;
+    init_playlist();
+  }
+}
